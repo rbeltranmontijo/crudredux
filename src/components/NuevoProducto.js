43ee7de088382b25a205678b8f66fbd7abb25e8c
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-17 22:53:59 -0600

    configurando los actions

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,6 +1,29 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
+//Actions de  Redux
+import { crearNuevoProductoAction } from "../actions/productoActions";
+
 const NuevoProducto = () => {
+  // Utilizar useDispatch y te crea una funcion
+  const dispatch = useDispatch();
+
+  // Mandar llamar el actio de productoAction
+  const agregarProducto = () => dispatch(crearNuevoProductoAction());
+
+  // Cuadno el usuario haga submit
+  const submitNuevoProducto = e => {
+    e.preventDefault();
+
+    // Validar Formulario
+
+    // Si no hay errores
+
+    // Crear el nuevo producto
+    agregarProducto();
+  };
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-8">
@@ -9,7 +32,7 @@ const NuevoProducto = () => {
             <h2 className="text-center mb-4 font-weight-bold">
               Agregar Nuevo Producto
             </h2>
-            <form>
+            <form onSubmit={submitNuevoProducto}>
               <div className="form-group">
                 <label htmlFor="">Nombre Producto</label>
                 <input
