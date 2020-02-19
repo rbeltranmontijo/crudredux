67a8084b3f2fda743362a60951ca1e9706092a38
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-18 22:40:32 -0600

    configurando el action para traer los productos

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -1,7 +1,10 @@
 import {
   AGREGAR_PRODUCTO,
   AGREGAR_PRODUCTO_EXITO,
-  AGREGAR_PRODUCTO_ERROR
+  AGREGAR_PRODUCTO_ERROR,
+  COMENZAR_DESCARGA_PRODUCTOS,
+  DESCARGA_PRODUCTOS_EXITO,
+  DESCARGA_PRODUCTOS_ERROR
 } from "../types";
 import clienteAxios from "../config/axios";
 import Swal from "sweetalert2";
@@ -50,4 +53,16 @@ const agregarProductoExito = producto => ({
 const agregarProductoError = estado => ({
   type: AGREGAR_PRODUCTO_ERROR,
   payload: estado
+});
+
+// Funcion descarga productos
+export function obtenerProductosAction() {
+  return async dispatch => {
+    dispatch(descargarProductos());
+  };
+}
+
+const descargarProductos = () => ({
+  type: COMENZAR_DESCARGA_PRODUCTOS,
+  payload: true
 });
