52bb88367ba373dfd1b94ed1db6d6a4c189f2595
Robin Beltran <rbeltranmontijo@gmail.com>
2020-02-19 21:53:21 -0600

    cambio de link a button con funcion usehistory

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Swal from "sweetalert2";
 
 // Redux
@@ -9,6 +9,8 @@ import { borrarProductoAction } from "../actions/productoActions";
 const Producto = ({ producto }) => {
   const { nombre, precio, id } = producto;
   const dispatch = useDispatch();
+
+  const history = useHistory(); // Habilitar history para redireccion
 
   // Confirmar si desea eliminar producto
   const confirmarEliminarProducto = id => {
@@ -29,6 +31,12 @@ const Producto = ({ producto }) => {
       }
     });
   };
+
+  // Funcion que redirige de forma programada
+  const redireccionarEdicion = producto => {
+    history.push(`/productos/editar/${producto.id}`);
+  };
+
   return (
     <tr>
       <td>{nombre}</td>
@@ -36,9 +44,13 @@ const Producto = ({ producto }) => {
         <span className="font-weight-bold">$ {precio}</span>
       </td>
       <td className="acciones">
-        <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">
+        <button
+          type="button"
+          onClick={() => redireccionarEdicion(producto)}
+          className="btn btn-primary mr-2"
+        >
           Editar
-        </Link>
+        </button>
         <button
           type="button"
           className="btn btn-danger"
